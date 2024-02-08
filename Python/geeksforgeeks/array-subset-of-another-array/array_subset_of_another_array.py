ae1da275d2914a1b932132d9e078f7264c664161
Mostefa-Terbeche <termus96@gmail.com>
2024-02-08 20:08:20 +0100

    solve array subset of another array

---
+++ Python/geeksforgeeks/array-subset-of-another-array/array_subset_of_another_array.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+def isSubset( a1, a2, n, m):
+    for number in a2:
+        if a1.count(number) < a2.count(number):
+            return ("No")
+            
+    return ("Yes")
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        sz = [int(x) for x in input().strip().split()]
+        n, m = sz[0], sz[1]
+        a1 = [int(x) for x in input().strip().split()]
+        a2 = [int(x) for x in input().strip().split()]
+        
+        print(isSubset( a1, a2, n, m))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
