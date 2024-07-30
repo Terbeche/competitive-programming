c12ac88fc9aa3860942dd85f16e7b56c9b1dfb6b
Mostefa-Terbeche <termus96@gmail.com>
2024-07-30 20:51:09 +0100

    Solve course schedule 2

---
+++ Python/Leetcode/Course-Schedule-II/course_schedule_II.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(numCourses)]
+        incoming = [0 for _ in range(numCourses)]
+        queue = deque()
+        order = []
+
+        for course, pre in prerequisites:
+            graph[pre].append(course)
+            incoming[course] += 1
+
+        for course in range(numCourses):
+            if incoming[course] == 0:
+                queue.append(course)
+
+        while queue:
+            course = queue.popleft()
+            order.append(course)
+            for neighbor in graph[course]:
+                incoming[neighbor] -= 1
+                if incoming[neighbor] == 0:
+                    queue.append(neighbor)
+
+        if len(order) != numCourses:
+            return []
+        
+        return order
