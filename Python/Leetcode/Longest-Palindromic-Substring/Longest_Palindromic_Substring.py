87800020fcbdf45637fb3a66267e022b60890017
Mostefa-Terbeche <termus96@gmail.com>
2024-08-27 00:15:54 +0100

    Solve Longest Palindromic Substring

---
+++ Python/Leetcode/Longest-Palindromic-Substring/Longest_Palindromic_Substring.py
@@ -0,0 +1,28 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        
+        start = 0
+        max_length = 1
+        
+        for i in range(n):
+            dp[i][i] = True
+        
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                start = i
+                max_length = 2
+        
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    if length > max_length:
+                        start = i
+                        max_length = length
+        
+        return s[start:start + max_length]
