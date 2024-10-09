7d218ea90f4f7b9b25b8b638013a52f7859051c7
Mostefa-Terbeche <termus96@gmail.com>
2024-10-09 20:51:33 +0100

    Solve Sort an Array

---
+++ Python/Leetcode/Sort-an-Array/Sort_an_Array.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        if n <= 1:
+            return nums
+    
+        mid = len(nums) // 2
+        left = self.sortArray(nums[:mid])
+        right = self.sortArray(nums[mid:])
+    
+        return merge(left, right)
+
+    def merge(left, right):
+        result = []
+        i, j = 0, 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        
+        result.extend(left[i:])
+        result.extend(right[j:])
+        
+        return result
